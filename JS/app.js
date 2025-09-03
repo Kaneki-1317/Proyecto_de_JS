1f8df82cdf31a1e91c872fcf716e7a2f5225cdaa
cristian <cristiandres1317@gmail.com>
2025-09-03 12:33:14 -0500

    feat: :zap: Aplicando el DOM

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@ const urlApi = "https://fakestoreapi.com/products"
 //Visualización del api en el html
 fetch(urlApi)
 .then(response => response.json())
-.then(data => {
+.then(data => {   
     for (let i = 0; i <= 20; i++){
         const contenedor = document.getElementById(`producto${i + 1}`)
         const producto = data[i];
@@ -14,7 +14,7 @@ fetch(urlApi)
                 <img class="img-productos" src="${producto.image}">
                 <h2 class="title">${producto.title}</h2>
                 <p class="precio">$${producto.price}</p>
-                <button>Añadir</button>
+                <button class="btn-carrito" data-id="${producto.id}">Añadir</button>
             `;
         }
     }
@@ -28,4 +28,37 @@ function mostarCarito(){
     }else{
         carrito.style.visibility = "hidden";
     }
+}
+
+const todosProductos = document.getElementById("todosProductos");
+
+let productsArray = [];
+
+document.addEventListener("DOMContentLoaded", function(){
+    eventListener();
+})
+
+function eventListener() {
+    todosProductos.addEventListener("click", getDataElements);
+}
+
+function getDataElements(e){
+    if (e.target.classList.contains("btn-carrito")){
+        const elementHtml = e.target.parentElement;
+        selectData(elementHtml);
+    }
+}
+
+function selectData(prod){
+    const productObj = {
+        img: prod.querySelector("img").src,
+        title: prod.querySelector("h2").textContent,
+        price: parseFloat(prod.querySelector("p").textContent.replace("$", "")),
+        id: parseInt(prod.querySelector(".btn-carrito").dataset.id, 10),
+        quantity: 1
+    }
+
+    productsArray = [...productsArray, productObj]
+
+    productsHtml();
 }
